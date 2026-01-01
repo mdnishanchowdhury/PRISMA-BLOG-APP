93083dc8180d8271e6e789bced8906d0e81b4df5
Md Nishan Chowdhury <mdnishanchowdhury.cse@gmail.com>
2026-01-01 12:25:04 +0600

    Add search functionality to posts API

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -21,7 +21,10 @@ const createPost = async (req: Request, res: Response) => {
 
 const getAllPort = async (req: Request, res: Response) => {
     try {
-        const result = await postService.getAllPost();
+        const { search } = req.query;
+        // console.log("search",search)
+        const searchString = typeof search === 'string' ? search : undefined;
+        const result = await postService.getAllPost({ search: searchString });
 
         res.status(200).json(result);
     } catch (error) {
